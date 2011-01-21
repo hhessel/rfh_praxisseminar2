3d2d2f8074e59e423458bde7f37e30855471900d
Henrik P. Hessel <hhessel@gmail.com>
2011-01-21 20:44:12 +0100

    -

--- kurse.php
+++ kurse.php
@@ -20,23 +20,29 @@ $kurse = $db->model('kurse')->select('*')->orderBy('semester')->execute()->resul
 if(count($kurse) == 0) {
 	$kurs_uebersicht = "Keine Kurse vorhanden";
 } else {
-		$kurs_uebersicht = '<table style="width:500px;"><tr><td style="width:30px;">Sem.</td><td style="width:250px;">Kursname</td><td style="width:100px;"></td><td width="*"></td></tr>';
+		$kurse_uebersicht = '<table style="width:500px;"><tr><td style="width:30px;">Sem.</td><td style="width:250px;">Kursname</td><td style="width:100px;"></td><td width="*"></td></tr>';
 		
 		while (list(, $kurs) = each($kurse)) {
 			$attachment_count = $db->model('attachment')->count()->where('id', $kurs['id'])->execute()->result;
-			$kurs_uebersicht .= '<tr>
+			$kurs_uebersicht = '<tr>
 									<td style="text-align:right;">' .  $kurs['semester'] . '</td>
 									<td>' . $kurs['kursname'] . '</td>
-									<td style="vertical-align:middle;">' . $attachment_count . ' <img src="images/note.png" style="vertical-align:top;"></td>
-									<td></td>
-								</tr>';
+									<td style="vertical-align:middle;">' . $attachment_count . '
+										<a href="kurse.php?view='. $kurs['id'] . '"><img src="images/note.png" style="vertical-align:top;"></a>
+										<img src="images/note_add.png" style="vertical-align:top;">
+									</td>';
+									
+			$kurs_uebersicht .= $userHandler->isAdmin() ? '<td>Edit | Delete </td>' : '<td></td>';
+			
+			$kurs_uebersicht .= '</tr>';				
+			$kurse_uebersicht .= $kurs_uebersicht;
 		}
 		
-		$kurs_uebersicht .= '</table>';
+		$kurse_uebersicht .= '</table>';
 		
 }	
 
-$templater->data(array("kurs_uebersicht" => $kurs_uebersicht));
+$templater->data(array("kurs_uebersicht" => $kurse_uebersicht));
 
 echo $templater->show();
 
